33ddf300081466fd16d49e83e30832a28cb47019
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-13 13:27:56 +0330

    [UPDATE] cookies vs sessions vs localStorage

--- src/chapter/storing-user-data/storing-user-data.chapter.tsx
+++ src/chapter/storing-user-data/storing-user-data.chapter.tsx
@@ -1,31 +1,45 @@
-import {ReactElement} from 'react';
+import { ReactElement } from "react";
 
-import cookiesImage from '@/assets/images/storing-user-data/cookies.webp';
-import cookiesMessageImage from '@/assets/images/storing-user-data/cookies-message.webp';
-import cookiesVsSessionsVsLocalstorageImage from '@/assets/images/storing-user-data/cookies-vs-sessions-vs-localstorage.webp';
-import rememberMeImage from '@/assets/images/storing-user-data/remember-me.webp';
+import comparisonTableImage from "@/assets/images/storing-user-data/comparison-table.webp";
+import cookiesMessageImage from "@/assets/images/storing-user-data/cookies-message.webp";
+import cookiesVsSessionsVsLocalstorageImage from "@/assets/images/storing-user-data/cookies-vs-sessions-vs-localstorage.webp";
+import cookiesImage from "@/assets/images/storing-user-data/cookies.webp";
+import rememberMeImage from "@/assets/images/storing-user-data/remember-me.webp";
 
-import ImageSlide, {ImageSize} from '@/slides/image/image.slide';
+import ImageSlide, { ImageSize } from "@/slides/image/image.slide";
 
 export default function StoringUserDataChapter(): ReactElement {
-    const heading = 'Storing User Data' as const;
+  const heading = "Storing User Data" as const;
 
-    return (
-        <section>
-            <ImageSlide
-                heading={heading}
-                imageSrc={cookiesMessageImage.src}
-                imageSize={ImageSize.MEDIUM}
-                caption="Cookies Message"
-            />
-            <ImageSlide heading={heading} imageSrc={cookiesImage.src} caption="How Cookies Work?" />
-            <ImageSlide heading={heading} imageSrc={cookiesVsSessionsVsLocalstorageImage.src} caption="" />
-            <ImageSlide
-                heading={heading}
-                imageSrc={rememberMeImage.src}
-                imageSize={ImageSize.MEDIUM}
-                caption="Remember Me"
-            />
-        </section>
-    );
+  return (
+    <section>
+      <ImageSlide
+        heading={heading}
+        imageSrc={cookiesMessageImage.src}
+        imageSize={ImageSize.MEDIUM}
+        caption="Cookies Message"
+      />
+      <ImageSlide
+        heading={heading}
+        imageSrc={cookiesImage.src}
+        caption="How Cookies Work?"
+      />
+      <ImageSlide
+        heading={heading}
+        imageSrc={cookiesVsSessionsVsLocalstorageImage.src}
+        caption="3 Tipical Ways"
+      />
+      <ImageSlide
+        heading={heading}
+        imageSrc={comparisonTableImage.src}
+        caption="Comparison"
+      />
+      <ImageSlide
+        heading={heading}
+        imageSrc={rememberMeImage.src}
+        imageSize={ImageSize.MEDIUM}
+        caption="Remember Me"
+      />
+    </section>
+  );
 }
