2eb3b8cfff93ba50122efd355e6d1f6bb965b2e8
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-20 12:07:59 +0330

    [CI/CD][UPDATE] output to export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/auth",
-  output: "standalone",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
